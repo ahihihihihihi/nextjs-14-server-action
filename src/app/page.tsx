1e5aa92f6a61eb7cca3830cd641ad487620e2a05
ahihi <bdsthocu20@gmail.com>
2023-12-01 15:01:34 +0700

    #208

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
-
 export default function Home() {
+
+  const handleLogin = async (formData: FormData) => {
+    'use server'
+    console.log(">>> check formData: username ", formData.get('username'))
+    console.log(">>> check formData: password ", formData.get('password'))
+
+  }
+
   return (
-    <div>Hello World!</div>
+    <div style={{ marginLeft: "200px" }}>
+      <h2>HTML Forms</h2>
+      <form action={handleLogin}>
+        <label >Username:</label>
+        <br />
+        <input type="text" name="username" />
+        <br />
+        <br />
+        <label >Password:</label>
+        <br />
+        <input type="text" name="password" />
+        <br /><br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   )
 }
