e33891dfabc8f7110847c77dc5127c79ebe312e8
ahihi <bdsthocu20@gmail.com>
2023-12-01 17:21:17 +0700

    #212

--- src/app/login/components/login.form.tsx
+++ src/app/login/components/login.form.tsx
@@ -1,14 +1,9 @@
 'use client'
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState } from 'react';
+import { Button, Form, Input } from 'antd';
+import { handleLoginAntd } from '@/app/users/actions';
+import { message } from 'antd'
 
-const onFinish = (values: any) => {
-    console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-};
 
 type FieldType = {
     username?: string;
@@ -16,39 +11,55 @@ type FieldType = {
     remember?: string;
 };
 
-const LoginForm = () => (
-    <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600, marginTop: "50px" }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item<FieldType>
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+const LoginForm = () => {
+
+    const [isSubmit, setIsSubmit] = useState(false);
+    const onFinish = async (values: any) => {
+        setIsSubmit(true)
+        const res = await handleLoginAntd(values);
+        if (res?.statusCode === 201) {
+            message.success(`${res?.message} | Login succeed !`)
+        } else {
+            message.error(res?.message)
+        }
+
+        console.log('res:', res);
+        setIsSubmit(false)
+    };
+
+    return (
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600, marginTop: "50px" }}
+            onFinish={onFinish}
+        // autoComplete="off"
         >
-            <Input />
-        </Form.Item>
+            <Form.Item<FieldType>
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input />
+            </Form.Item>
 
-        <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-            <Input.Password />
-        </Form.Item>
+            <Form.Item<FieldType>
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
 
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>
-);
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    )
+}
 
 export default LoginForm;
